77f17b13b200d2ed528bab6f541b233fb44e376f
gthielon <gthielon@gmail.com>
2016-12-14 23:14:32 +0100

    conflit ok

--- src/FindMeBundle/Controller/PartieController.php
+++ src/FindMeBundle/Controller/PartieController.php
@@ -4,11 +4,10 @@ namespace FindMeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class PartieController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('FindMeBundle:Default:index.html.twig');
+        return $this->render('FindMeBundle:Default:partie.html.twig');
     }
-
 }
