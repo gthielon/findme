957ba1722c0007ff255c4449aaf887a48ab2cf13
louisemunier <munier.louise0@gmail.com>
2016-12-14 19:38:18 +0100

    Added mapping relation for Level

--- src/FindMeBundle/Entity/Level.php
+++ src/FindMeBundle/Entity/Level.php
@@ -22,4 +22,50 @@ class Level
     {
         return $this->id;
     }
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $levels;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->levels = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add level
+     *
+     * @param \FindMeBundle\Entity\Game $level
+     *
+     * @return Level
+     */
+    public function addLevel(\FindMeBundle\Entity\Game $level)
+    {
+        $this->levels[] = $level;
+
+        return $this;
+    }
+
+    /**
+     * Remove level
+     *
+     * @param \FindMeBundle\Entity\Game $level
+     */
+    public function removeLevel(\FindMeBundle\Entity\Game $level)
+    {
+        $this->levels->removeElement($level);
+    }
+
+    /**
+     * Get levels
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getLevels()
+    {
+        return $this->levels;
+    }
 }
