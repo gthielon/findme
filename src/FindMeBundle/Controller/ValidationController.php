59b36df4f217aae091bb931b27905eba5d526774
louisemunier <munier.louise0@gmail.com>
2016-12-15 15:40:36 +0100

    Little css changes in page profil

--- src/FindMeBundle/Controller/ValidationController.php
+++ src/FindMeBundle/Controller/ValidationController.php
@@ -1,29 +1,26 @@
 <?php
-/*
-namespace MarvelBundle\Controller;
+
+namespace FindMeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use FindMeBundle\Entity\User;
+use FindMeBundle\Entity\Player;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Constraint;
 
 class ValidationController extends Controller{
 
-    public function validationAction(User $user, Request $request){
-        $em = $this->getDoctrine()->getManager();
-        $user = $em->getRepository('FindMeBundle:User')->findOneById($id);
-        //Possible de récupérer les données avec Request $request en paramètre, spécifique à SF
-        $validation = 0;
-        if ($validation == 1){
-            $winner = 1;
-            return $this->render('@FindMe/Default/partie.html.twig', array(
-                'user' => $user,
-                'points' => $points,
-            ));
-        }
-        else {
-            return $this->render('@FindMe/Default/partie.html.twig', array(
-                'id' => $id,
-            ));
-        }
+//        si validation -> recupérer id user + id game    et les lier
+//                      -> ajouter des pts au user
+
+    public function validationAction()
+    {
+        $validator = Validation::createValidator();
+
+
+        return $this->render('FindMeBundle::validation.html.twig');
+
+
     }
-}*/
+}
