aab090ee5f46c7fe55c13a2eeacdff838bf20dba
gthielon <gthielon@gmail.com>
2016-12-16 07:48:09 +0100

    Ajout points

--- src/FindMeBundle/Controller/ProfilController.php
+++ src/FindMeBundle/Controller/ProfilController.php
@@ -4,12 +4,28 @@ namespace FindMeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use FindMeBundle\Entity\Player;
 
 class ProfilController extends Controller
 {
     public function indexAction()
     {
         return $this->render('@FindMe/profil.html.twig');
+    }
+
+    /**
+     * Lists all player entities.
+     *
+     */
+    public function profilAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $player = $em->getRepository('FindMeBundle:Player')->findAll();
+
+        return $this->render('FindMeBundle::profil.html.twig', array(
+            'player' => $player,
+        ));
     }
 
 //    Add points to winner when the game is validated
