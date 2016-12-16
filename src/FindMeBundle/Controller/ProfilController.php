b38ed7cea583edf739eea721ee653749e7420fe1
gthielon <gthielon@gmail.com>
2016-12-16 09:57:14 +0100

    Remise points en dur

--- src/FindMeBundle/Controller/ProfilController.php
+++ src/FindMeBundle/Controller/ProfilController.php
@@ -13,20 +13,6 @@ class ProfilController extends Controller
         return $this->render('@FindMe/profil.html.twig');
     }
 
-    /**
-     * Lists all player entities.
-     *
-     */
-    public function profilAction()
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $player = $em->getRepository('FindMeBundle:Player')->findAll();
-
-        return $this->render('FindMeBundle::profil.html.twig', array(
-            'player' => $player,
-        ));
-    }
 
 //    Add points to winner when the game is validated
 //    public function addPoints(Request $request)
